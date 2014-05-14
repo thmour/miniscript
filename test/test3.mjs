13914acdda37c9a0557fcefc419c140f768540df
theofilos93 <mtheofilos@gmail.com>
2014-05-15 01:05:39 +0300

    for loops

--- test/test3.mjs
+++ test/test3.mjs
@@ -1,4 +1,14 @@
-arr = [1,2,3,4,5]
-i = -1
-
-while arr:[++i]
+obj = { a=1 b=2 c=3 }
+            db = [{ user='peter' age=21 }, { user='john' age=32 }, { user='ralph' age=27 }]
+            for i of 1..5
+	            print i
+            ;
+            for i in obj
+	            print i + ':', obj:i
+            ;
+            String::capitalize = # {
+                return this:0.toUpperCase() + this.slice(1)
+            }
+            for user, age in db
+                print "%user.capitalize() is %{age + 1} years old" //Don't tell anyone!
+            ;
