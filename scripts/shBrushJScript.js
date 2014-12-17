da53ceaad2627f896520bc0aa212fb4ab383330b
theofilos93 <mtheofilos@gmail.com>
2014-12-17 13:02:35 +0200

    New version v0.4

--- scripts/shBrushJScript.js
+++ scripts/shBrushJScript.js
@@ -1,32 +1,16 @@
-/**
- * SyntaxHighlighter
- * http://alexgorbatchev.com/SyntaxHighlighter
- *
- * SyntaxHighlighter is donationware. If you are using it, please donate.
- * http://alexgorbatchev.com/SyntaxHighlighter/donate.html
- *
- * @version
- * 3.0.83 (July 02 2010)
- * 
- * @copyright
- * Copyright (C) 2004-2010 Alex Gorbatchev.
- *
- * @license
- * Dual licensed under the MIT and GPL licenses.
- */
 ;(function()
 {
 	// CommonJS
-	typeof(require) != 'undefined' ? SyntaxHighlighter = require('shCore').SyntaxHighlighter : null;
+	SyntaxHighlighter = SyntaxHighlighter || (typeof require !== 'undefined'? require('shCore').SyntaxHighlighter : null);
 
 	function Brush()
 	{
-		var keywords =	'break case catch continue ' +
-						'default delete do else false  ' +
-						'for function if in instanceof ' +
-						'new null return super switch ' +
-						'this throw true try typeof var while with'
-						;
+		var keywords =	'break case catch class continue ' +
+				'default delete do else enum export extends false  ' +
+				'for function if implements import in instanceof ' +
+				'interface let new null package private protected ' +
+				'static return super switch ' +
+				'this throw true try typeof var while with yield';
 
 		var r = SyntaxHighlighter.regexLib;
 		
@@ -35,6 +19,7 @@
 			{ regex: r.multiLineSingleQuotedString,					css: 'string' },			// single quoted strings
 			{ regex: r.singleLineCComments,							css: 'comments' },			// one line comments
 			{ regex: r.multiLineCComments,							css: 'comments' },			// multiline comments
+			{ regex: /\b[0-9]+(?:\.[0-9]+)?(?:e[+-]?[0-9]+)?/gm,	css: 'numbers'	},			// numbers
 			{ regex: /\s*#.*/gm,									css: 'preprocessor' },		// preprocessor tags like #region and #endregion
 			{ regex: new RegExp(this.getKeywords(keywords), 'gm'),	css: 'keyword' }			// keywords
 			];
@@ -43,7 +28,7 @@
 	};
 
 	Brush.prototype	= new SyntaxHighlighter.Highlighter();
-	Brush.aliases	= ['js', 'jscript', 'javascript'];
+	Brush.aliases	= ['js', 'jscript', 'javascript', 'json'];
 
 	SyntaxHighlighter.brushes.JScript = Brush;
 
--- scripts/shBrushMini.js
+++ scripts/shBrushMini.js
@@ -1,44 +1,42 @@
 ;(function()
 {
 	// CommonJS
-	typeof(require) != 'undefined' ? SyntaxHighlighter = require('shCore').SyntaxHighlighter : null;
+	SyntaxHighlighter = SyntaxHighlighter || (typeof require !== 'undefined'? require('shCore').SyntaxHighlighter : null);
 
 	function Brush()
 	{
-		var keywords_main =	'if for class switch try while do ' +
-							'elif else case default catch finally ' +
-							'typeof';
-							
-		var keywords_class = 'static public private';
-		
-		var keywords_op = 'as in of is and or not instanceof import print throw return exit new super set get has';
-		
-		var keywords_val = 'null undefined Infinity NaN this true false';
+		var keywords =	'if for index each class switch try while do ' +
+						'elif else end case default catch finally export ' +
+						'delete typeof func times break continue type ' +
+						'return import print write throw';
+
+		var operators = 'new and or is not in as instanceof typeof delete';
+
+		var constants = 'true false nil null undefined';
 
 		var r = SyntaxHighlighter.regexLib;
 		
 		this.regexList = [
-			{ regex: r.multiLineDoubleQuotedString, css: 'string'},
-			{ regex: r.multiLineSingleQuotedString,	css: 'string' },
-			{ regex: r.singleLineCComments,	css: 'comments' },
-			{ regex: /\s;|#|{|}/g, css: 'keyword' },
-			{ regex: /@?@[$_a-zA-Z0-9]+/g, css: 'this'},
-			{ regex: /\.[$_a-zA-Z][$_a-zA-Z0-9]*/g, css: 'member' },
-			{ regex: new RegExp(this.getKeywords(keywords_main), 'gm'), css: 'keyword' },
-			{ regex: new RegExp(this.getKeywords(keywords_class), 'gm'), css: 'keyword_class' },
-			{ regex: new RegExp(this.getKeywords(keywords_op), 'gm'), css: 'keyword_op' },
-			{ regex: new RegExp(this.getKeywords(keywords_val), 'gm'),	css: 'keyword_val' }
+			{ regex: r.multiLineDoubleQuotedString,								css: 'string'	},	// double quoted strings
+			{ regex: r.multiLineSingleQuotedString,								css: 'string'	},	// single quoted strings
+			{ regex: r.singleLineCComments,										css: 'comments' },	// one line comments
+			{ regex: r.multiLineCComments,										css: 'comments' },	// multiline comments
+			{ regex: /\b[0-9]+(?:\.[0-9]+)?(?:e[+-]?[0-9]+)?/gm,				css: 'numbers'	},	// numbers
+			{ regex: /\b[A-Z][$\w]*\b/g,										css: 'class'	},	// classes
+			{ regex: /:\s*\b[$\w]+\b/g,											css: 'string'	},	// parent class
+			{ regex: /\b(this|super)\b/g,										css: 'this'		},	// this/super
+			{ regex: new RegExp(this.getKeywords(keywords) + '\\s', 'gm'),		css: 'keyword' 	},	// keywords
+			{ regex: new RegExp(this.getKeywords(constants), 'gm'),				css: 'constants'},	// constants
+			{ regex: new RegExp(this.getKeywords(operators), 'gm'),				css: 'operators'},	// operators
 			];
 	
 		this.forHtmlScript(r.scriptScriptTags);
 	};
 
 	Brush.prototype	= new SyntaxHighlighter.Highlighter();
-	Brush.aliases	= ['mini'];
-	
-	
+	Brush.aliases	= ['mini', 'miniscript'];
 
-	SyntaxHighlighter.brushes.Mini = Brush;
+	SyntaxHighlighter.brushes.Miniscript = Brush;
 
 	// CommonJS
 	typeof(exports) != 'undefined' ? exports.Brush = Brush : null;
